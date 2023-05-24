9dc80b248b94ae630efac622a9e53a823a529f05
Sidney François <sidney.fvm.dev@outlook.com>
2023-05-24 17:30:05 -0300

    add first event handler(onChange) in Search component

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 
 function Search() {
+  const handleChange = () => {
+    console.log(event);
+    console.log(event.target.value);
+  };
+
   return (
     <>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
     </>
   );
 }
