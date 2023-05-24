354e24e3dabc4b9929ea5e1a1c2b96f88091ae19
Sidney François <sidney.fvm.dev@outlook.com>
2023-05-24 17:12:19 -0300

    basic jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import * as React from "react";
+
 function App() {
-  return <h1>Hello World</h1>;
+  const title = "React";
+
+  const welcome = {
+    greeting: "Hey",
+    title: "React",
+  };
+
+  function getTitle() {
+    return title;
+  }
+
+  return (
+    <>
+      <h1>Hello {title}</h1>
+
+      <h2>{getTitle()}</h2>
+      <h3>
+        {welcome.greeting}, {welcome.title}
+      </h3>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </>
+  );
 }
 
 export default App;
