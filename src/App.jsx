1cb48f2ede3cf87a0a77892885580143d5a13c77
Sidney François <sidney.fvm.dev@outlook.com>
2023-05-25 10:42:45 -0300

    use real API to fetch stories's list, remove fake async call and initial fake stories list

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,7 @@ import InputWithLabel from "./InputWithLabel";
 import useStorageState from "./Hooks/UseStorageState";
 import { useEffect, useState, useReducer } from "react";
 
-const initialStories = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
-  );
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
@@ -71,15 +49,17 @@ function App() {
   useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
-        setIsLoading(false);
       })
-      .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
+      .catch(() => {
+        dispatchStories({ type: "STORIES_FETCH_FAILURE" });
+      });
   }, []);
 
   const handleSearch = (event) => {
